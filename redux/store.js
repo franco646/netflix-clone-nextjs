691caf556bc7031c4259f1c8b34e5638236474b8
franco646 <franco@>
2022-07-06 22:36:52 -0300

    Create store.js

---
+++ redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { createWrapper } from "next-redux-wrapper";
+
+import modalReducer from "./reducers/modal.reducer";
+import moviesReducer from "./reducers/movies.reducer";
+
+// create your reducer
+const reducer = {
+  modal: modalReducer,
+  movies: moviesReducer,
+};
+
+const makeStore = (context) => configureStore({ reducer });
+
+export const wrapper = createWrapper(makeStore, { debug: true });
