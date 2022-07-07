7d10c10a829ac3bd2dad362c5176204fffad3057
franco646 <franco@>
2022-07-06 22:36:10 -0300

    Update moviesCarousel.js

--- components/moviesCarousel/moviesCarousel.js
+++ components/moviesCarousel/moviesCarousel.js
@@ -1,5 +1,7 @@
-import React, { useState, useRef, useLayoutEffect } from "react";
+import React, { useState, useRef, useLayoutEffect, useEffect } from "react";
 import Slider from "../slider/slider";
+
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 function useWindowSize() {
   const [size, setSize] = useState([0]);
@@ -15,10 +17,12 @@ function useWindowSize() {
   return size;
 }
 
-const MoviesCarousel = ({ movies, title }) => {
+const MoviesCarousel = ({ movies, title, myKey }) => {
   const sliderRef = useRef(null);
   const [sliderItems, setSliderItems] = useState([...movies]);
+  const [firstSliderItems, setFirstSliderItems] = useState([]);
   const [wasSlide, setWasSlide] = useState(false);
+  const [isMoving, setIsMoving] = useState(false);
 
   let moviesPerSlice = 5;
   const [width] = useWindowSize();
@@ -34,56 +38,82 @@ const MoviesCarousel = ({ movies, title }) => {
   }
 
   const handleClick = (direction) => {
-    sliderRef.current.style.transform = wasSlide
-      ? `translate(-200%)`
-      : `translate(-100%)`;
-
-    if (direction === "left") {
-      const lastElement = moviesCopy.slice(-moviesPerSlice);
-      sliderItems.unshift(lastElement);
-    } else {
-      if (wasSlide) {
-        const sliderItemsCopy = [...sliderItems];
-        const firstElements = sliderItemsCopy.splice(0, moviesPerSlice);
-        sliderItemsCopy.push.apply(sliderItemsCopy, firstElements);
-        setTimeout(() => {
-          sliderRef.current.style.transition = "none";
-          setSliderItems(sliderItemsCopy);
-          sliderRef.current.style.transform = `translate(-100%)`;
-          setTimeout(() => {
-            sliderRef.current.style.transition =
-              "transform .54s cubic-bezier(0.4, 0, 1, 1) 0s";
-          }, 60);
-        }, 540);
+    if (!isMoving) {
+      setIsMoving(true);
+      const sliderItemsCopy = [...sliderItems];
+      if (direction === "left") {
+        sliderRef.current.style.transform = `translate(calc(-100% / ${moviesPerSlice}))`;
+        const lastItems = sliderItemsCopy.splice(
+          sliderItems.length - moviesPerSlice,
+          sliderItems.length
+        );
+        sliderItemsCopy.unshift.apply(sliderItemsCopy, lastItems);
+      } else {
+        sliderRef.current.style.transform = wasSlide
+          ? `translate(calc(-200% - 100% / ${moviesPerSlice}))`
+          : "translate(-100%)";
+        const firstElements = sliderItemsCopy.splice(0, moviesPerSlice); // remove first items and saved
+        sliderItemsCopy.push.apply(sliderItemsCopy, firstElements); // push first items at the end
       }
-      setWasSlide(true);
+      setTimeout(() => {
+        sliderRef.current.style.transition = "none"; // remove the transition animation
+        setSliderItems(sliderItemsCopy); // set the updated slider items array
+        setWasSlide(true);
+        sliderRef.current.style.transform = `translate(calc(-100% - 100% / ${moviesPerSlice}))`;
+      }, 540);
+      setTimeout(() => {
+        setIsMoving(false);
+        sliderRef.current.style.transition =
+          "transform .54s cubic-bezier(0.4, 0, 1, 1) 0s"; // add the transition animation
+      }, 600);
     }
   };
 
+  useEffect(() => {
+    if (wasSlide) {
+      const lastItems = sliderItems.slice(
+        sliderItems.length - moviesPerSlice - 1,
+        sliderItems.length
+      );
+      setFirstSliderItems(lastItems);
+    }
+  }, [moviesPerSlice, sliderItems, wasSlide]);
+
   return (
-    <div className="overflow-hidden">
-      <h1 className="text-white my-4">{title}</h1>
-      <button className="text-white" onClick={() => handleClick("right")}>
-        mover
-      </button>
-      <div className="">
-        <Slider movies={sliderItems} myRef={sliderRef} />
+    <div className="overflow-hidden my-[3vw]" key={myKey}>
+      <h2 className="text-white font-bold leading-[1.4vw] text-xs lg:text-[1.4vw] mx-[4%] mb-2 mt-0 lg:mt-1">
+        {title}
+      </h2>
+      <div className="relative group">
+        {wasSlide ? (
+          <button
+            className="absolute rounded-r-sm text-white hover: bg-carousel-button hover:bg-carousel-button-hover left-0  top-0 bottom-[calc(0px+2px)] w-[calc(4%-2px)] z-10"
+            onClick={() => handleClick("left")}
+          >
+            <BsChevronLeft className="hidden group-hover:block hover:scale-125 h-full w-full" />
+          </button>
+        ) : null}
+        <div
+          className={`${
+            isMoving ? "pointer-events-none" : "pointer-events-auto"
+          }`}
+        >
+          <Slider
+            movies={sliderItems}
+            firstItems={firstSliderItems}
+            myRef={sliderRef}
+            moviesPerSlice={moviesPerSlice}
+          />
+        </div>
+        <button
+          className="absolute rounded-l-sm text-white bg-carousel-button hover:bg-carousel-button-hover right-0  top-0 bottom-[calc(0px+2px)] w-[calc(4%-2px)] z-10"
+          onClick={() => handleClick("right")}
+        >
+          <BsChevronRight className="hidden group-hover:block hover:scale-125 h-full w-full" />
+        </button>
       </div>
     </div>
   );
 };
-
-/**
- * <div className='overflow-x-visible scrollbar-hide' ref={sliderRef}>
-        <div className="w-[500%] flex shrink-0" >
-          
-        <div className="basis-[250%] md:basis-[calc(500%/3)] lg:basis-[calc(500%/4)] xl:basis-[calc(500%/5)] flex shrink-0 transition duration-200 ease-out">
-          {movies?.map((movie, i) => {
-            return <SliderItem movie={movie} key={i} />;
-          })}
-        </div>
-        </div>
-        </div>
- */
 
 export default MoviesCarousel;
