e9ae974f1183835a9e82df473fb89b53891ffdb1
franco646 <franco@>
2022-07-07 20:23:35 -0300

    Update slider.js

--- components/slider/slider.js
+++ components/slider/slider.js
@@ -1,20 +1,25 @@
 import SliderItem from "../sliderItem/sliderItem";
 
-const Slider = ({ movies, moviesPerSlice, myRef, firstItems }) => {
+const Slider = ({ movies, moviesPerSlice, wasSlide, myRef }) => {
   return (
     <div className="px-[4%] relative touch-pan-y z-0">
       <div className="overflow-x-visible ease-in duration-[.54s]" ref={myRef}>
         <div className="whitespace-nowrap">
-          {firstItems?.map((movie, i) => {
-            return <SliderItem movie={movie} key={i} />;
-          })}
-          {movies?.map((movie, i) => {
+          {movies?.slice(0, moviesPerSlice * 3 + 2).map((movie, i) => {
             return (
               <SliderItem
                 movie={movie}
                 key={i}
                 corner={
-                  i === 0 ? "left" : i === moviesPerSlice - 1 ? "right" : ""
+                  wasSlide && i === moviesPerSlice + 1
+                    ? "left"
+                    : !wasSlide && i === 0
+                    ? "left"
+                    : wasSlide && i === moviesPerSlice * 2
+                    ? "right"
+                    : !wasSlide && i === moviesPerSlice - 1
+                    ? "right"
+                    : ""
                 }
               />
             );
