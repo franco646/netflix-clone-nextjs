5073fcbd1337a8ca3c6ca1a3905f817ff1153881
franco646 <franco@>
2022-07-06 22:35:52 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import fetchMoviesByTrending from "./trendingMovies";
+import fetchNetflixOriginalSeries from "./netflixOriginals";
+import { fetchMoviesGenres, fetchMoviesByGenre } from "./moviesByGenre";
+
+export {
+  fetchMoviesGenres,
+  fetchMoviesByGenre,
+  fetchMoviesByTrending,
+  fetchNetflixOriginalSeries,
+};
