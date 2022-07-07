efc8bb878fa8b7cd4fbbb5e4c98cc720b685a0b9
franco646 <franco@>
2022-07-06 22:36:32 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import React from "react";
+import { wrapper } from "../redux/store";
+
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
