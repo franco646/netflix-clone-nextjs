6768e36f6f519593f96d1706f1b8d89cc94ea659
franco646 <franco@>
2022-07-24 18:57:32 -0300

    Update trendingMovies.js

--- api/trendingMovies.js
+++ api/trendingMovies.js
@@ -3,11 +3,11 @@ import axios from "./baseAxios";
 const API_KEY = "9b91ed4496735d11d8b7b6da24043385";
 
 const fetchMoviesByTrending = async (
-  apiCallType = "all",
+  apiCallType = "all", // all, tv or movies
   title = "Tendencias"
 ) => {
   const response = await axios.get(
-    `https://api.themoviedb.org/3/trending/${apiCallType}/week?api_key=${API_KEY}`
+    `https://api.themoviedb.org/3/trending/${apiCallType}/day?api_key=${API_KEY}`
   );
   return [{ title, movies: response.data.results }];
 };
