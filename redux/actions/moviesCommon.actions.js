575910b32654d34230f56e0178c358d12f64d6d2
franco646 <franco@>
2022-07-24 18:57:47 -0300

    Update moviesCommon.actions.js

--- redux/actions/moviesCommon.actions.js
+++ redux/actions/moviesCommon.actions.js
@@ -5,6 +5,9 @@ import {
   fetchNetflixOriginalSeries,
   fetchMoviesByGenre,
   fetchMoviesGenres,
+  fetchTvSeriesGenres,
+  fetchTvSeriesByGenre,
+  fetchUpcomingMovies,
 } from "../../api";
 
 export const moviesRequest = createAction("movies/Request");
@@ -28,6 +31,66 @@ export const setMoviesGenres = createAction(
   (genres) => ({
     payload: genres,
   })
+);
+
+export const setBrowseLink = createAction(
+  "movies/setBrowseLink",
+  (browseLink) => ({
+    payload: browseLink,
+  })
+);
+
+export const getTvSeries = createAsyncThunk(
+  "movies/getTvSeries",
+  async (_, { dispatch }) => {
+    dispatch(moviesRequest());
+    try {
+      const genres = await fetchTvSeriesGenres();
+      dispatch(setMoviesGenres(genres));
+      const tvSeriesByGenre = await fetchTvSeriesByGenre(genres);
+      dispatch(moviesRequestSuccess(tvSeriesByGenre));
+    } catch (error) {
+      dispatch(moviesRequestFailure);
+    }
+  }
+);
+
+export const getMovies = createAsyncThunk(
+  "movies/getMovies",
+  async (_, { dispatch }) => {
+    dispatch(moviesRequest());
+    try {
+      const genres = await fetchMoviesGenres();
+      dispatch(setMoviesGenres(genres));
+      const moviesByGenre = await fetchMoviesByGenre(genres);
+      dispatch(moviesRequestSuccess(moviesByGenre));
+    } catch (error) {
+      dispatch(moviesRequestFailure);
+    }
+  }
+);
+
+export const getTopMovies = createAsyncThunk(
+  "movies/getTopMovies",
+  async (_, { dispatch }) => {
+    dispatch(moviesRequest());
+    try {
+      const topMovies = await fetchMoviesByTrending(
+        "movie",
+        "Las películas más populares hoy"
+      );
+      const topTvSeries = await fetchMoviesByTrending(
+        "tv",
+        "Las series más populares hoy"
+      );
+      const upcomingMovies = await fetchUpcomingMovies();
+      dispatch(
+        moviesRequestSuccess([...topMovies, ...topTvSeries, ...upcomingMovies])
+      );
+    } catch (error) {
+      dispatch(moviesRequestFailure);
+    }
+  }
 );
 
 export const getBrowseMovies = createAsyncThunk(
