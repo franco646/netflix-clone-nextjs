feddfc87426619a775cd6dcf74b55f4b69cde197
franco646 <franco@>
2022-07-24 18:57:44 -0300

    Update sliderItem.js

--- components/sliderItem/sliderItem.js
+++ components/sliderItem/sliderItem.js
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
 
 import { connect } from "react-redux";
 
@@ -25,12 +25,16 @@ const SliderItem = ({ movie, corner, showModal, setModalData }) => {
       onMouseLeave={cancelModalOpening}
       className="relative inline-block box-border whitespace-normal align-top w-6/12 h-[calc((100vw-8vw)/2*0.562)] md:w-4/12 md:h-[calc((100vw-8vw)/3*0.562)] lg:w-3/12 lg:h-[calc((100vw-8vw)/4*0.562)] xl:w-1/5 xl:h-[calc((100vw-8vw)/5*0.562)] rounded overflow-hidden px-0.5"
     >
-      <img
-        alt="Mountains"
-        src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
-        layout="fill"
-        objectfit="contain"
-      />
+      <div className='relative h-full w-full'>
+        <Image
+          placeholder='blur'
+          blurDataURL={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
+          alt="Mountains"
+          src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
+          layout="fill"
+          objectfit="contain"
+        />
+      </div>
     </div>
   );
 };
