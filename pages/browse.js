f78ba4a570f318aa9466370f252be37d32da2e41
franco646 <franco@>
2022-07-24 18:57:45 -0300

    Update browse.js

--- pages/browse.js
+++ pages/browse.js
@@ -1,29 +1,52 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Modal from "../components/modal/modal";
 import MoviesCarousel from "../components/moviesCarousel/moviesCarousel";
 import BrowseNavbar from "../components/navbars/browseNavbar";
 
 import { connect } from "react-redux";
 
-import { getBrowseMovies } from "../redux/actions/moviesCommon.actions";
+import {
+  getBrowseMovies,
+  getMovies,
+  getTvSeries,
+  getTopMovies,
+} from "../redux/actions/moviesCommon.actions";
 
-const Browse = ({ getMovies, moviesByGenre, isLoading, error }) => {
+const Browse = ({
+  getBrowseMovies,
+  getTvSeries,
+  getMovies,
+  getTopMovies,
+  browseLink,
+  moviesByGenre,
+}) => {
   useEffect(() => {
-    getMovies();
-  }, [getMovies]);
+    if (browseLink === "/") {
+      getBrowseMovies();
+    }
+    if (browseLink === "/series") {
+      getTvSeries();
+    }
+    if (browseLink === "/movies") {
+      getMovies();
+    }
+    if (browseLink === "/top") {
+      getTopMovies();
+    }
+  }, [getBrowseMovies, browseLink, getTvSeries, getMovies, getTopMovies]);
 
   return (
     <div className="bg-netflix-bg-gray min-h-screen">
       <BrowseNavbar />
       <div>
         {moviesByGenre?.map((genre, index) => (
-          <MoviesCarousel
-            myKey={index}
-            key={index}
-            movies={genre.movies}
-            title={genre.title}
-          />
-        ))}
+            <MoviesCarousel
+              myKey={index}
+              key={index}
+              movies={genre.movies}
+              title={genre.title}
+            />
+          ))}
       </div>
       <Modal />
     </div>
@@ -31,13 +54,17 @@ const Browse = ({ getMovies, moviesByGenre, isLoading, error }) => {
 };
 
 const mapStateToProps = (state) => ({
+  browseLink: state.movies.browseLink,
   moviesByGenre: state.movies.movies,
   isLoading: state.movies.isLoading,
   error: state.movies.error,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  getMovies: () => dispatch(getBrowseMovies()),
+  getBrowseMovies: () => dispatch(getBrowseMovies()),
+  getTvSeries: () => dispatch(getTvSeries()),
+  getMovies: () => dispatch(getMovies()),
+  getTopMovies: () => dispatch(getTopMovies()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Browse);
