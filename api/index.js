3e05a1724c37e96f0e8362d4c1949bdbec73aeb0
franco646 <franco@>
2022-07-24 18:57:20 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import fetchMoviesByTrending from "./trendingMovies";
 import fetchNetflixOriginalSeries from "./netflixOriginals";
+import fetchUpcomingMovies from "./upcomingMovies";
 import { fetchMoviesGenres, fetchMoviesByGenre } from "./moviesByGenre";
+import { fetchTvSeriesGenres, fetchTvSeriesByGenre } from "./seriesByGenre";
 
 export {
   fetchMoviesGenres,
   fetchMoviesByGenre,
+  fetchTvSeriesGenres,
+  fetchTvSeriesByGenre,
+  fetchUpcomingMovies,
   fetchMoviesByTrending,
   fetchNetflixOriginalSeries,
 };
