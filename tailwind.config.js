175da3e5ffce8db9447d7c7943be768b273d5cde
franco646 <franco@>
2022-07-24 23:54:07 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
         "3xl": "rgba(0, 0, 0, 75%) 0px 3px 10px",
       },
       colors: {
+        navbar: "rgb(20, 20, 20)",
         "netflix-red": "#e50914",
         "netflix-bg-gray": "#141414",
         "gray-1": "#b3b3b3",
@@ -39,6 +40,12 @@ module.exports = {
         "carousel-button-hover": "rgba(20, 20, 20, 0.7)",
       },
       backgroundImage: {
+        "navbar-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, .7) 10%, transparent)",
+        "billboar-bottom-gradient":
+          "linear-gradient(180deg,hsla(0,0%,8%,0) 0,hsla(0,0%,8%,.15) 15%,hsla(0,0%,8%,.35) 29%,hsla(0,0%,8%,.58) 44%,#141414 68%,#141414)",
+        "billboard-gradient":
+          "linear-gradient(77deg, rgba(0, 0, 0, .6), transparent 85%)",
         "background-gradient":
           "linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%);",
       },
