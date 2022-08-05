873e04073ce879379becbffc4dd8f07cd703bd89
franco646 <franco@>
2022-08-04 23:51:52 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useStore } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import { wrapper } from "../redux/store";
 
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => {
+  const store = useStore((state) => state);
+  return (
+    <PersistGate loading={<div>loading</div>} persistor={store.__persistor}>
+      <Component {...pageProps} />
+    </PersistGate>
+  );
+};
 
 export default wrapper.withRedux(MyApp);
