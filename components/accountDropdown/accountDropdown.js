b7fe2617a757c875ac2404e589f78c579269a531
franco646 <franco@>
2022-08-04 23:51:07 -0300

    Update accountDropdown.js

--- components/accountDropdown/accountDropdown.js
+++ components/accountDropdown/accountDropdown.js
@@ -1,29 +1,41 @@
 import { AiFillCaretDown } from "react-icons/ai";
 
+import { connect } from "react-redux";
+
+import {
+  userLogout,
+  userSelectProfile,
+} from "../../redux/actions/user.actions";
+
 import ProfilePicture from "../profilePicture/profilePicture";
 
-const AccountDropdown = ({}) => {
+const AccountDropdown = ({ user, logout, selectProfile }) => {
   return (
     <div className="group">
       <div className="flex">
-        <ProfilePicture src="/profile-picture-1.png" />
+        <ProfilePicture src={user?.selectedProfile?.picture} />
         <AiFillCaretDown className="relative ml-1 text-white h-8 group-hover:rotate-180 duration-150" />
       </div>
-      <div className="hidden group-hover:block group-hover:absolute right-%5 lg:right-14 md:right-11 py-2.5 leading-8 border-gray-3  text-sm border-[1px] min-w-[180px] bg-login-card">
-        <div className="flex p-1.5 cursor-pointer">
-          <ProfilePicture src="/profile-picture-1.png" />
-          <div className="text-white ml-2.5 hover:underline">primer</div>
-        </div>
-        <div className="flex p-1.5 cursor-pointer">
-          <ProfilePicture src="/profile-picture-2.png" />
-          <div className="text-white ml-2.5 hover:underline">Franco</div>
-        </div>
-        <div className="flex p-1.5 cursor-pointer">
-          <ProfilePicture src="/profile-picture-3.png" />
-          <div className="text-white ml-2.5 hover:underline">segundo</div>
-        </div>
+      <div className="invisible group-hover:visible absolute right-%5 lg:right-14 md:right-11 py-2.5 leading-8 border-gray-3 delay-100  text-sm border-[1px] min-w-[180px] bg-login-card top-[52px]">
+        {user?.profiles
+          .filter((profile) => profile.id !== user.selectedProfile.id)
+          .map((profile, i) => (
+            <div
+              className="flex p-1.5 cursor-pointer"
+              key={i}
+              onClick={() => selectProfile(profile)}
+            >
+              <ProfilePicture src={profile.picture} />
+              <div className="text-white ml-2.5 hover:underline">
+                {profile.name}
+              </div>
+            </div>
+          ))}
         <hr className="my-2" />
-        <div className="text-white hover:underline cursor-pointer leading-3 text-center">
+        <div
+          className="text-white hover:underline cursor-pointer leading-3 text-center"
+          onClick={logout}
+        >
           Cerrar sesión en Netflix
         </div>
       </div>
@@ -31,4 +43,13 @@ const AccountDropdown = ({}) => {
   );
 };
 
-export default AccountDropdown;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(userLogout()),
+  selectProfile: (profile) => dispatch(userSelectProfile(profile)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountDropdown);
